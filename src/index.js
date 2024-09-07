345f90e7a6710143aa71f934fe53d75dd57dcb59
mikhacavin <mikhacavinarthur@gmail.com>
2024-09-07 09:15:37 +0700

    updatedddd

--- src/index.js
+++ src/index.js
@@ -11,3 +11,23 @@ if ("serviceWorker" in navigator) {
     });
 } else {
 }
+
+let defferedPrompt;
+const addbtn = document.querySelector(".btn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  defferedPrompt = event;
+  addbtn.style.display = "block";
+});
+
+addbtn.addEventListener("click", (event) => {
+  defferedPrompt.prompt();
+
+  defferedPrompt.userChoice.then((choice) => {
+    if (choice.outcome === "accepted") {
+      console.log("user accepted the prompt");
+    }
+    defferedPrompt = null;
+  });
+});
